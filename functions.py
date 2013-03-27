9bea97a48e7bef719e132dbaec39e67df88be36f
thekenner33 <k.bambridge@me.com>
2013-03-27 02:07:50 -0400

    Minor fixes for choosing tweets

--- functions.py
+++ functions.py
@@ -2,7 +2,11 @@ import urllib
 import simplejson
 import random
 
+
 def searchTweets(query):
     search = urllib.urlopen("http://search.twitter.com/search.json?q=lang%3Aen%20"+query)
     dict = simplejson.loads(search.read())
-    return random.choice(dict["results"])["text"]
+    x = "\n"
+    while ('\n' in x):
+        x = random.choice(dict["results"])["text"]
+    return x
